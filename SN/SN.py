d9969ad8e954342acc07dca5d49531d3600d481b
StellaBYR <63144812+StellaBYR@users.noreply.github.com>
2022-05-10 11:17:56 +0800

    add no game market

--- SN/SN.py
+++ SN/SN.py
@@ -31,7 +31,7 @@ def cal_pM(theta, rho, lambda_, m, score):
         lambda_reciprocal_sum += 1 / lambda_[i]
     cons_C = rho[1] * score / 4 * lambda_reciprocal_sum
     cons_D = score * score / 2 / theta[1] * lambda_reciprocal_sum
-    pM = (-cons_D + math.sqrt(cons_D * cons_D + 4 * cons_C *cons_D)) / (2 * cons_C * cons_D)
+    pM = (-cons_D + math.sqrt(cons_D * cons_D + 4 * cons_C * cons_C * cons_D)) / (2 * cons_C * cons_D)
     return pM
 
 
@@ -126,6 +126,7 @@ def Stackelberg_Nash_DataMarket(x_test, y_test,#test_data
     true_score = cut_r2_score(y_test, y_pred)
     if true_score < 0:
         true_score = 0
+  
     qM = cal_qM(chi, tau, m, true_score)
     qD = cal_qD(chi, tau, m)
     Phi = cal_Phi(chi, tau, true_score, theta, m, pM, qM, rho)#buyer
@@ -154,16 +155,126 @@ def Stackelberg_Nash_DataMarket(x_test, y_test,#test_data
     for i in range(m):
         new_omega[i] /= max_seller_shapley
         new_omega[i] = new_omega[i] * omega_rate + omega[i] * (1 - omega_rate)
+    cost=cal_traincost(N, score, sigma)
     return Phi, Omega, Psi, new_omega, pD, pM, tau, true_score
     #return profits and refresh omega(weight)
 
 
-def No_Game_Market(x_test, y_test,#test_data
+def No_Game_Market_pM(x_test, y_test,#test_data
                     theta, rho, score,#buyer
                     sigma,#broker
                     lambda_, omega, m, N, x_in, y_in,#seller
                     model, omega_rate,
-                    pD, pM, tau):#strategy
+                    pM):#strategy
+    tau_coef = cal_tau_coef(omega, lambda_, N, m)
+    #pM = cal_pM(theta, rho, lambda_, m, score)
+    pD = pM * score / 2
+    tau = tau_coef * pD
+    
+    epss = np.zeros(m)
+    for i in range(m):
+        epss[i] = cal_eps_from_tau(tau[i])
+    chi = cal_chi(omega, tau, m, N)
+
+    #generate train data based on chi and epsilon
+    x_train = np.zeros((N, len(x_in[0][0])))
+    y_train = np.zeros(N)
+    idx = 0
+    for i in range(m):
+        for j in range(int(chi[i])):
+            x_train[idx] = OneD_DP(x_in[i][j], epss[i])
+            y_train[idx] = y_in[i][j] + LapNoise(epss[i])
+            idx += 1
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+    true_score = cut_r2_score(y_test, y_pred)
+    if true_score < 0:
+        true_score = 0
+    qM = cal_qM(chi, tau, m, true_score)
+    qD = cal_qD(chi, tau, m)
+    Phi = cal_Phi(chi, tau, true_score, theta, m, pM, qM, rho)#buyer
+    Omega = cal_Omega(pM, qM, pD, qD, N, true_score, sigma)#broker
+    Psi = np.zeros(m)
+    for i in range(m):
+        Psi[i] = cal_Psi_i(pD, tau[i], chi[i], lambda_[i])
+    data_shapley = mc_shap(x_train, y_train, x_test, y_test, model, 100)
+    min_data_shapley = np.min(data_shapley)
+    new_omega = np.zeros(m)
+    idx = 0
+    for i in range(m):
+        for j in range(int(chi[i])):
+            new_omega[i] += data_shapley[idx] - min_data_shapley
+            idx += 1
+    max_seller_shapley = np.max(new_omega)
+    for i in range(m):
+        new_omega[i] /= max_seller_shapley
+        new_omega[i] = new_omega[i] * omega_rate + omega[i] * (1 - omega_rate)
+    return Phi, Omega, Psi, new_omega, true_score
+    #return profits and refresh omega(weight)
+
+def No_Game_Market_pD(x_test, y_test,#test_data
+                    theta, rho, score,#buyer
+                    sigma,#broker
+                    lambda_, omega, m, N, x_in, y_in,#seller
+                    model, omega_rate,
+                    pD):#strategy
+    tau_coef = cal_tau_coef(omega, lambda_, N, m)
+    pM = cal_pM(theta, rho, lambda_, m, score)
+    #pD = pM * score / 2
+    tau = tau_coef * pD
+    
+    epss = np.zeros(m)
+    for i in range(m):
+        epss[i] = cal_eps_from_tau(tau[i])
+    chi = cal_chi(omega, tau, m, N)
+
+    #generate train data based on chi and epsilon
+    x_train = np.zeros((N, len(x_in[0][0])))
+    y_train = np.zeros(N)
+    idx = 0
+    for i in range(m):
+        for j in range(int(chi[i])):
+            x_train[idx] = OneD_DP(x_in[i][j], epss[i])
+            y_train[idx] = y_in[i][j] + LapNoise(epss[i])
+            idx += 1
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+    true_score = cut_r2_score(y_test, y_pred)
+    if true_score < 0:
+        true_score = 0
+    qM = cal_qM(chi, tau, m, true_score)
+    qD = cal_qD(chi, tau, m)
+    Phi = cal_Phi(chi, tau, true_score, theta, m, pM, qM, rho)#buyer
+    Omega = cal_Omega(pM, qM, pD, qD, N, true_score, sigma)#broker
+    Psi = np.zeros(m)
+    for i in range(m):
+        Psi[i] = cal_Psi_i(pD, tau[i], chi[i], lambda_[i])
+    data_shapley = mc_shap(x_train, y_train, x_test, y_test, model, 100)
+    min_data_shapley = np.min(data_shapley)
+    new_omega = np.zeros(m)
+    idx = 0
+    for i in range(m):
+        for j in range(int(chi[i])):
+            new_omega[i] += data_shapley[idx] - min_data_shapley
+            idx += 1
+    max_seller_shapley = np.max(new_omega)
+    for i in range(m):
+        new_omega[i] /= max_seller_shapley
+        new_omega[i] = new_omega[i] * omega_rate + omega[i] * (1 - omega_rate)
+    return Phi, Omega, Psi, new_omega, true_score
+    #return profits and refresh omega(weight)
+
+def No_Game_Market_tau(x_test, y_test,#test_data
+                    theta, rho, score,#buyer
+                    sigma,#broker
+                    lambda_, omega, m, N, x_in, y_in,#seller
+                    model, omega_rate,
+                    tau):#strategy
+    #tau_coef = cal_tau_coef(omega, lambda_, N, m)
+    pM = cal_pM(theta, rho, lambda_, m, score)
+    pD = pM * score / 2
+    ##tau = tau_coef * pD
+    
     epss = np.zeros(m)
     for i in range(m):
         epss[i] = cal_eps_from_tau(tau[i])
@@ -266,3 +377,4 @@ def inner_compare_Market(x_test, y_test,#test_data
         new_omega[i] = new_omega[i] * omega_rate + omega[i] * (1 - omega_rate)
     return Phi, Omega, Psi, new_omega, pD, pM, tau, true_score
     #return profits and refresh omega(weight)
+
